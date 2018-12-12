fa023330cae8009c059d74565d479e553d309cd7
vainamoinen <weixinluwx@foxmail.com>
2018-12-13 01:45:42 +0800

    none

---
+++ src/dynamic_loop/whole_loop.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import json
+import numpy as np
+import time
+import datetime
+
+from utils.env import *
+from utils.data_handler import DataHandler as dh
+
+def loop(params, G, embeddings, weights, metric, output_path, draw):
+    params["get_next"]["input_file"] = os.path.join(DATA_PATH, params["get_next"]["input_file"])
+    module_next = __import__(
+            "get_next." + params["get_next"]["func"], fromlist = ["get_next"]).GetNext
+    period_id=0
+    gn = module_next(params["get_next"])
+
+    params_new = params["new_embedding"]
+    module_new_embedding = __import__(
+            "init_embedding." + params_new["func"], fromlist = ["new_embedding"]).NodeEmbedding
+    def new_embedding(G, init_embeddings, init_weights, n):
+        ne = module_new_embedding(params_new, G)
+        embeddings, weights = ne.train()
+        return embeddings, weights
+    time_path = output_path + "_time"
+    dynamic_embeddings = []
+    while True:
+        period_id += 1
+        if period_id==8:
+            break
+        num_new = gn.get_next(G)
+        print("nodes_num after get_next:",G.number_of_nodes())
+        if num_new == 0:
+            break
+        st = datetime.datetime.now()
+        embeddings, weights = new_embedding(G, embeddings, weights, num_new)
+        ed = datetime.datetime.now()
+        dh.append_to_file(time_path, str(ed - st) + "\n")
+        res = metric(embeddings)
+        draw(embeddings)
+        dynamic_embeddings.append({"embeddings": embeddings.tolist(), "weights": weights.tolist()})
+
+
+    with open(output_path + "_dynamic", "w") as f:
+        f.write(json.dumps(dynamic_embeddings))
+
---
+++ src/get_next/get_next_static.py
@@ -0,0 +1,104 @@
+import networkx as nx
+import os
+import sys
+
+
+class GetNext(object):
+    def __init__(self, params):
+        print("get_next init begin")
+        self.f = open(params["input_file"], "r")
+        self.is_directed = params["is_directed"]
+        self.num_to_del = params["num_to_del"]
+        self.num_at_least = params["num_at_least"]
+
+        print("get_next init done")
+
+    @staticmethod
+    def dict_del(d, key, num_del):
+        # print("d:",d,"key:",key,"value:",d[key])
+        if key in d:
+            d[key] -= num_del
+        else:
+            d[key] -= num_del
+
+    @staticmethod
+    def dict_add(d, key, add):
+        if key in d:
+            d[key] += add
+        else:
+            d[key] = add
+
+    def get_next(self, G):
+        print("get_next function begin")
+        # self.get_next_times-=1
+        # # if self.get_next_times<0:
+        #     return 0
+        num_nodes_pre = G.number_of_nodes()
+        num_to_del = self.num_to_del
+
+        for num in xrange(num_to_del):
+            print("num_to_del loop num:",num)
+            line = self.f.readline()
+            if not line:
+                return num
+            line = line.strip()
+            if len(line) == 0:
+                line = self.f.readline()
+                if not line:
+                    return num
+                line = line.strip()
+            node_to_del, connected_nodes_num = [int(i) for i in line.split()]
+
+            for i in xrange(connected_nodes_num):
+                line = self.f.readline()
+                line = line.strip()
+                u, v = [int(i) for i in line.split()]
+                # print("node id:" + str(u) + " its out_degree is")
+                GetNext.dict_del(G.node[u], 'out_degree', 1)
+                # print("node id:" + str(v) + " its in_degree is")
+                GetNext.dict_del(G.node[v], 'in_degree', 1)
+                G.graph['degree'] -= 1
+                #G.remove_edge(u,v)
+                if not self.is_directed and u != v:
+                    # print("node id:" + str(v) + " its out_degree is")
+                    GetNext.dict_del(G.node[v], 'out_degree', 1)
+                    # print("node id:" + str(u) + " its in_degree is")
+                    GetNext.dict_del(G.node[u], 'in_degree', 1)
+                    #G.remove_edge(v,u)
+                    G.graph['degree'] -= 1
+            G.remove_node(node_to_del)
+
+        print("get_next function done")
+        return num_to_del
+
+    # def get_next(self, G):
+    #     for num in xrange(self.n):
+    #         while True:
+    #             line = self.f.readline()
+    #             if not line:
+    #                 return num
+    #             line = line.strip()
+    #             if len(line) == 0:
+    #                 continue
+    #             u, m = [int(i) for i in line.split()]
+    #             break
+    #
+    #         for i in xrange(m):
+    #             line = self.f.readline()
+    #             line = line.strip()
+    #             u, v = [int(i) for i in line.split()]
+    #             G.add_edge(u, v, weight=1)
+    #             GetNext.dict_add(G.node[u], 'out_degree', 1)
+    #             GetNext.dict_add(G.node[v], 'in_degree', 1)
+    #             GetNext.dict_add(G.graph, 'degree', 1)
+    #             if not self.is_directed and u != v:
+    #                 G.add_edge(v, u, weight=1)
+    #                 GetNext.dict_add(G.node[v], 'out_degree', 1)
+    #                 GetNext.dict_add(G.node[u], 'in_degree', 1)
+    #                 G.graph['degree'] += 1
+    #
+    #     return self.n
+
+    def __del__(self):
+        self.f.close()
+
